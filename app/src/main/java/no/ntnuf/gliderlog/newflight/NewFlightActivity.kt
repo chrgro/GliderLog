777887089678e97e81222869c95c3ed33169c030
Christian Grøvdal <chrgro@gmail.com>
2026-04-10 21:52:11 +0200

    Coloring fixes, fixing autocomplete of notes.

--- app/src/main/java/no/ntnuf/gliderlog/newflight/NewFlightActivity.kt
+++ app/src/main/java/no/ntnuf/gliderlog/newflight/NewFlightActivity.kt
@@ -5,7 +5,7 @@ import android.app.TimePickerDialog
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.preference.PreferenceManager
+import androidx.preference.PreferenceManager
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.MenuItem
